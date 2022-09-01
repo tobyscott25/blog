6d45a0f8ab3b72a0a5ea7ab53a632970af01b318
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-09-01 22:00:33 +1000

    Add FontAwesome Icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+library.add(fas, fab, far)
+config.autoAddCss = false
+
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconLookup, IconDefinition, findIconDefinition } from '@fortawesome/fontawesome-svg-core'
+
+const githubLookup: IconLookup = { prefix: 'fab', iconName: 'github' }
+const githubIconDefinition: IconDefinition = findIconDefinition(githubLookup)
+
+const twitterLookup: IconLookup = { prefix: 'fab', iconName: 'twitter' }
+const twitterIconDefinition: IconDefinition = findIconDefinition(twitterLookup)
+
+
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
@@ -20,12 +31,18 @@ const Home: NextPage = () => {
 				</div>
 
 				<a href="https://github.com/tobyscott25" className={styles.card} target="_blank">
-					<div className="text-xl">GitHub &rarr;</div>
+					<div className="text-xl">
+						<FontAwesomeIcon icon={githubIconDefinition} className='mr-3' />
+						GitHub &rarr;
+					</div>
 					<div className="text-lg">Projects, contributions &amp; dotfiles</div>
 				</a>
 
 				<a href="https://twitter.com/tobyscott25" className={styles.card} target="_blank">
-					<div className="text-xl">Twitter &rarr;</div>
+					<div className="text-xl">
+					<FontAwesomeIcon icon={twitterIconDefinition} className='mr-3' />
+						Twitter &rarr;
+					</div>
 					<div className="text-lg">Thoughts and opinions</div>
 				</a>
 				
